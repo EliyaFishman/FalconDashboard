e59a76d530693b75fdf8216a5f62bd9ebecb7179
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2020-02-09 18:56:20 +0200

    bug fixes and fullscreen

--- src/main/kotlin/org/ghrobotics/falcondashboard/MainView.kt
+++ src/main/kotlin/org/ghrobotics/falcondashboard/MainView.kt
@@ -3,6 +3,7 @@ package org.ghrobotics.falcondashboard
 import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil
 import javafx.scene.Parent
 import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyCombination
 import javafx.scene.input.KeyEvent
 import kfoenix.jfxtabpane
 import org.ghrobotics.falcondashboard.generator.GeneratorView
@@ -11,7 +12,7 @@ import tornadofx.*
 
 class MainView : View("FRC 5190 Falcon Dashboard") {
     override val root: Parent = hbox {
-        keyboard {
+        jfxtabpane {
             addEventHandler(KeyEvent.KEY_PRESSED) {
                 when {
                     it.code == KeyCode.S && it.isControlDown -> {
@@ -24,12 +25,10 @@ class MainView : View("FRC 5190 Falcon Dashboard") {
                     it.code == KeyCode.O && it.isControlDown -> Saver.load()
                     it.code == KeyCode.G && it.isControlDown -> FalconDashboard.trajectory =
                         TrajectoryUtil.serializeTrajectory(GeneratorView.trajectory.value)
+                    it.code == KeyCode.F4 -> FX.primaryStage.let { stage -> stage.isFullScreen = !stage.isFullScreen }
                 }
             }
-        }
-        jfxtabpane {
             stylesheets += resources["/AppStyle.css"]
-
             prefHeight = 705.0
             prefWidth = 1550.0
 
@@ -42,5 +41,10 @@ class MainView : View("FRC 5190 Falcon Dashboard") {
                 isClosable = false
             }
         }
+    }
+
+    init {
+        FX.primaryStage.fullScreenExitKeyCombination = KeyCombination.NO_MATCH
+        FX.primaryStage.fullScreenExitHint = "Press F4 to exit"
     }
 }
